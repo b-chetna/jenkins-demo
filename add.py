21c82a62a1fc19cb529b032524e5da81043a638d
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-09-03 10:58:20 +0530

    gvgbhnmj,kkmjnhbv

--- add.py
+++ add.py
@@ -3,7 +3,7 @@ def add(a,b):
     return a+b
 if __name__=="__main__":
     a = int(sys.argv[1])
-    b = int(sys.argv[1])
+    b = int(sys.argv[2])
     result = add(a,b)
     print("============================")
     print("         Addition           ")
